560332fe4fc4e858f638b69faf80a03df5b3f79f
Jonathan Brooks <jonbrooks0122@gmail.com>
2023-10-22 19:41:39 -0700

    server code for todo app

--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -2,28 +2,47 @@
 
 const { users } = require('../models/index.js');
 
-const bearer = async (req, res, next) => {
-  if (!req.headers.authorization) return next('Invalid Login');
+// const bearer = async (req, res, next) => {
+//   if (!req.headers.authorization) return next('Invalid Login');
 
+//   try {
+//     const [token, authType] = req.headers.authorization.split(' ');
+
+//     if (authType === 'Bearer') {
+//       let validUser = await users.authenticateBearer(token);
+//       if (validUser) {
+//         req.user = validUser;
+//         req.token = validUser.token;
+//         return next();
+//       } else {
+//         res.status(403).send('Invalid Login');
+//       }
+//     } else {
+//       res.status(403).send('Invalid Login');
+//     }
+//   } catch (e) {
+//     console.error(e);
+//     res.status(403).send('Invalid Login');
+//   }
+// };
+
+// module.exports = bearer;
+
+module.exports = async (req, res, next) => {
   try {
-    const [token, authType] = req.headers.authorization.split(' ');
-
-    if (authType === 'Bearer') {
-      let validUser = await users.authenticateBearer(token);
-      if (validUser) {
-        req.user = validUser;
-        req.token = validUser.token;
-        return next();
-      } else {
-        res.status(403).send('Invalid Login');
-      }
-    } else {
-      res.status(403).send('Invalid Login');
+    if (!req.headers.authorization) {
+      next('Invalid Login');
     }
+
+    const token = req.headers.authorization.split(' ').pop();
+    const validUser = await users.authenticateToken(token);
+    // console.log('valid user', validUser);
+
+    req.user = validUser;
+    req.token = validUser.token;
+    next();
   } catch (e) {
     console.error(e);
     res.status(403).send('Invalid Login');
   }
 };
-
-module.exports = bearer;
